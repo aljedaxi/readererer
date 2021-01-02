dba2d02ce167778e87b9e664fe7b75fe551fd504
jacob whitford-bender <jacob@xmentium.com>
2021-01-02 12:04:53 -0700

    calendar rendering fixes for selected day

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import {
 } from 'react';
 import {
 	range, 
+	div,
 	map, 
 	add,
 	pipe,
 	sub,
 	I,
+	any, fromMaybe,
+	isNothing,
+	parseFloat,
 } from 'sanctuary';
 import {
 	formatWithOptions, getISODay, compareAsc, differenceInCalendarDays
@@ -20,9 +24,9 @@ const getDaysInM = y => m => new Date(y,m,0).getDate();
 const succ = add (1);
 const trace = s => {console.log(s); return s;};
 
-const CalBox = ({children, size = 150, ...rest}) => {
+const CalBox = ({children, size = 150, style, ...rest}) => {
 	return (
-		<div {...rest} style={{width: size, height: size}}>
+		<div {...rest} style={{width: size, height: size, ...style}}>
 			{children}
 		</div>
 	);
@@ -30,7 +34,6 @@ const CalBox = ({children, size = 150, ...rest}) => {
 
 const useDayStyles = createUseStyles({
 	container: {
-		background: props => props.selected ? 'red' : undefined,
 		'&:hover': {
 			background: props => props.hoverColor,
 		}
@@ -98,26 +101,42 @@ const Menu = props => {
 	const [rate, setRate] = useState('');
 	const handleSubmit = e => {
 		e.preventDefault();
-		onDone({pages});
+		const hopefullyBoth = map (parseFloat) ({pages, rate});
+		if (any (isNothing) (hopefullyBoth)) {
+			alert('a horse walks into a cafe and asks for coffee with no milk. the barista says "we don\'t have coffee with no milk, but i can give you coffee with no cream".');
+			return;
+		}
+		onDone(map (fromMaybe (0)) (hopefullyBoth));
 	};
 	const row = {width: '100%'};
 	return (
 		<form onSubmit={handleSubmit}>
-			<label htmlFor="pages">how many pages of actual content?</label>
-			<input id='pages' name='pages' type="text" onChange={setter(setPages)} value={pages} />
-			<label htmlFor="rate">how many pages can you read in an hour?</label>
-			<input id='rate' name='rate' type="text" onChange={setter(setRate)} value={rate} />
-			<input type="submit" />
+			<div style={row}>
+				<label htmlFor="pages">how many pages of actual content?</label>
+				<input id='pages' name='pages' type='number' onChange={setter(setPages)} value={pages} />
+			</div>
+			<div style={row}>
+				<label htmlFor="rate">how many pages can you read in an hour?</label>
+				<input id='rate' name='rate' type='number' onChange={setter(setRate)} value={rate} />
+			</div>
+			<div style={row}>
+				<input type="submit" />
+			</div>
 		</form>
 	);
 };
 
 const steps = [Menu, Calendar];
-const capitalize = s => `${s[0].toUpperCase()}${s.slice(1)}`;
+const calcHoursPerDay = hours => days => {
+	if (days <= 1) return hours;
+	if (hours <= 0 || days <= 0) return 0;
+	return hours / days;
+};
+
 const App = () => {
 	const [step, setStep] = useState(0);
-	const [pages, setPages] = useState(500);
-	const [pagesPerHour, setPagesPerHour] = useState(50);
+	const [pages, setPages] = useState(0);
+	const [pagesPerHour, setPagesPerHour] = useState(0);
 	const [day, setDay] = useState();
 
 	const today = startOfToday();
@@ -126,22 +145,23 @@ const App = () => {
 	const hoverColor = 'yellow';
 
 	const hours = pages / pagesPerHour;
-	const hourPerDay = hours / (differenceInCalendarDays (today) (day) - 1);
+	const daysTilDue = differenceInCalendarDays (today) (day);
+	const hourPerDay = calcHoursPerDay (hours) (daysTilDue);
 	const showHours = n => {
-		if (!day) return false;
+		if (!day || compareAsc (today) (day) < 1) return false;
 		const thisDay = new Date(year, month, n);
 		const c2this = compareAsc (thisDay);
 		return (c2this (day) === 1 && c2this (today) < 1)
 			|| c2this (today) === 0;
 	};
 
-	const setters = {setStep, setPages, setPagesPerHour, setDay};
-	const setAll = p => Object.entries(p).map(([k,v]) => setters[`set${capitalize(k)}`](v));
-	const title = 'when\'s your thing?';
+	const title = step === 0 ? 'tell me about yourself' : 'when\'s your thing?';
 	const mainProps = {
 		pages, pagesPerHour, day, month, year,
-		onDone: p => {
-			setAll(p); setStep(add(1));
+		onDone: ({pages, rate}) => {
+			setPages(pages);
+			setPagesPerHour(rate);
+			setStep(add(1));
 		},
 		onDayClicked: n => setDay(new Date(year, month, n)),
 		Day: props => {
@@ -151,10 +171,14 @@ const App = () => {
 			} = props;
 			const selected = compareAsc (new Date(year, month, n)) (day) === 0;
 			const text = showHours (n) ? hourPerDay.toFixed(1) : '';
-			const {container} = useDayStyles({hoverColor, selected});
+			const {container} = useDayStyles({hoverColor});
+			const style = {
+				color: selected ? 'red' : undefined,
+				borderRight: 'black',
+			};
 			const handleClick = _ => (onClick ?? I)(n);
 			return (
-				<CalBox className={container} onClick={handleClick} key={n}>
+				<CalBox style={style} className={container} onClick={handleClick} key={n}>
 					<h4>
 						{n}
 						<br />
