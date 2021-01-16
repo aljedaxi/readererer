4c0a602491c17eb37280455193f4cc8837b7297d
jacob whitford-bender <jacob@xmentium.com>
2021-01-16 16:35:53 -0700

    can create a custom time for each day

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,39 @@
-import {useState,} from 'react';
+import {createElement, Fragment, useEffect, useState} from 'react';
 import {
+	snd,
 	add,
 	any,
 	append,
 	complement,
 	compose,
+	concat,
 	div,
 	elem,
 	filter,
+	fromPairs,
+	keys,
+	lift2,
 	map,
 	maybeToNullable,
+	mult,
+	Pair,
+	pairs,
 	parseFloat,
+	parseInt as overlyComplexForThisSituationParseInt,
 	pipe,
 	range,
+	reject,
 	sub,
+	sum,
 } from 'sanctuary';
 import {
 	compareAsc,
-	differenceInCalendarDays,
+	eachDayOfInterval,
 	eachMonthOfInterval,
 	formatWithOptions,
 	getISODay,
 	getMonth,
+	getUnixTime,
 	getYear,
 	parseISO,
 } from 'date-fns/fp';
@@ -29,6 +41,8 @@ import {startOfToday} from 'date-fns';
 import * as locales from 'date-fns/locale';
 import {Combobox, ComboboxInput, ComboboxList, ComboboxOption, ComboboxPopover,} from "@reach/combobox";
 import "@reach/combobox/styles.css";
+
+const parseInt = overlyComplexForThisSituationParseInt (10);
 
 const getDaysInM = y => m => new Date(y,m,0).getDate();
 const succ = add (1);
@@ -180,6 +194,15 @@ const isntInRange = ({clickedDay, eDay, today, isReadingLastMinute}) => {
 	return f (clickedDay) (eDay) || isEarlierThan (today) (clickedDay);
 };
 
+// parseMinutes :: String -> Maybe Int
+const parseMinutes = s => parseInt(s.slice(0, -1));
+// parseTime :: String -> Maybe Int
+const parseTime = s => {
+	const onH = s.split('h');
+	return onH.length > 1
+		? lift2 (add) (parseMinutes(onH[1])) (map (mult (60)) (parseInt(onH[0])))
+		: parseMinutes(onH[0]);
+};
 const formatMinutes = uglyM => {
 	const m = Math.round(uglyM);
 	const hours = m >= 60 ? Math.floor(m / 60) : 0;
@@ -208,37 +231,73 @@ const dayCalcs = ({clickedDay, eDay, today, isReadingLastMinute, daysOff}) => {
 
 const jan27th = new Date(2021, 0, 27);
 
-const onDayBad = ({setDaysOff, eDay, daysOff, year, month, today, isReadingLastMinute}) => n => {
-	const clickedDay = new Date(year, month, n)
-	if (isntInRange({clickedDay, eDay, today, isReadingLastMinute})) return;
-	elem (clickedDay) (daysOff)
-		? setDaysOff(filter(complement (datesEq (clickedDay))))
-		: setDaysOff(append(clickedDay));
+const merge = o1 => o2 => Object.assign({}, o2, o1);
+const borderlessInput = {
+	border: 'none',
+  width: '5em',
 };
 
+const Hours = ({time, onSubmit}) => {
+	const [value, setValue] = useState(time);
+	const onChange = setter(setValue);
+	return (
+		<form onSubmit={onSubmit}>
+			<input style={borderlessInput} {...{value, onChange}}/>
+		</form>
+	)
+};
+
+const today = startOfToday();
 const App = () => {
 	const landscape = window.innerHeight < window.innerWidth;
-	const today = startOfToday();
 	// const hoverColor = '#ffff0099';
 	const size = landscape ? 150 : 50;
 
-	const [step, setStep] = useState(1);
+	const [step, setStep] = useState(0);
 	const incStep = _ => setStep(succ);
-	const [pages, setPages] = useState(1000);
+	const [pages, setPages] = useState(300);
 	const [rate, setRate] = useState(60);
-	const perMinute = rate / 60;
 	const [nameOfThing, setNameOfThing] = useState('Seminar');
 	const [eDay, setEDay] = useState(jan27th);
 	const [daysOff, setDaysOff] = useState([]);
-	const [isReadingLastMinute, setIsReadingLastMinute] = useState(1);
+	const [isReadingLastMinute, setIsReadingLastMinute] = useState(0);
+	const [readingTimes, setReadingTimes] = useState({});
+	const [customTimes, setCustomTimes] = useState({});
+	const addToCustomTimes = day => minutes => setCustomTimes(merge({[getUnixTime(day)]: minutes}));
 
-	const minutes = pages / perMinute;
-	const daysTilDue = differenceInCalendarDays (today) (eDay) - daysOff.length - isReadingLastMinute;
-	const timePerDay = calcTimePerDay (minutes) (daysTilDue);
+	useEffect(() => {
+		const elemOfDaysOf = d => (any (datesEq (d)) (daysOff));
+		const elemOfCustomTimes = d => (any (eq (getUnixTime (d))) (keys (customTimes)));
+		const or = f => g => x => f(x) || g(x);
+
+		const perMinute = rate / 60;
+		const minutes = (pages / perMinute);
+		const timestampsOff = map (getUnixTime) (daysOff);
+		const activeCustomTimes = fromPairs (reject (p => timestampsOff.includes(p)) (pairs (customTimes)));
+		const baseMinutes = minutes - sum(activeCustomTimes);
+
+		const possibleReadingDays = eachDayOfInterval({start: today, end: eDay})
+		const readingDays = reject (or (elemOfDaysOf) (elemOfCustomTimes)) (possibleReadingDays);
+
+		const timePerCustomDay = map (formatMinutes) (activeCustomTimes);
+
+		const daysTilDue = readingDays.length - daysOff.length - isReadingLastMinute;
+		const baseReadingTime = calcTimePerDay (baseMinutes) (daysTilDue);
+
+		const r = map (d => Pair (getUnixTime(d).toString()) (baseReadingTime)) (readingDays);
+		setReadingTimes(fromPairs(concat (r) (pairs (timePerCustomDay))));
+	}, [pages, rate, eDay, daysOff, isReadingLastMinute, customTimes]);
+
+	const handleSubmit = day => e => {
+		e.preventDefault();
+		const input = e.target[0];
+		const maybeNewTime = parseTime(input.value.trim());
+		map (addToCustomTimes (day)) (maybeNewTime);
+	};
 
 	const title = [
-		_ => 'tell me about yourself',
-		_ => `which days can't you read?`,
+		'tell me about yourself',
+		`which days can't you read?`,
 	][step];
 
 	const Day = props => {
@@ -247,16 +306,28 @@ const App = () => {
 		} = props;
 		const thisDay = new Date(year, month, n);
 		const {showHours, selected, isOff} = dayCalcs({clickedDay: thisDay, eDay, today, isReadingLastMinute, daysOff});
-		const handleClick = _ => onDayBad({today, setEDay, eDay, setDaysOff, year, month, daysOff, isReadingLastMinute}) (n);
+
+		const readingTimeThisDay = showHours && <Hours time={readingTimes[getUnixTime(thisDay)]} onSubmit={handleSubmit(thisDay)}/>;
+
+		const handleClick = _ => {
+			const clickedDay = new Date(year, month, n)
+			if (isntInRange({clickedDay, eDay, today, isReadingLastMinute})) return;
+			elem (clickedDay) (daysOff)
+				? setDaysOff(filter(complement(datesEq(clickedDay))))
+				: setDaysOff(append(clickedDay));
+		};
+		const buttonStuff = { onClick: handleClick, role: 'button', style: {cursor: 'pointer'}, title: `${isOff ? 'add' : 'remove'} this day from the reading days`};
+
 		const color = selected ? '#0080ff' : undefined;
-		const background = isOff ? '#ee002d99' : undefined;
-		const style = { color, background };
+		const titleProps = showHours || isOff ? buttonStuff : {};
 		return (
-			<CalBox size={size} style={style} className='realDay' onClick={handleClick} key={n}>
+			<CalBox size={size} style={{color}} className='realDay' key={n}>
 				<div style={{padding: 5}}>
-					<h4> {n} </h4>
+					<h4 {...titleProps}>
+						{createElement(isOff ? 's' : Fragment, {}, n)}
+					</h4>
 					{landscape ? <br /> : null}
-					{showHours && timePerDay}
+					{readingTimeThisDay}
 					{showHours && selected && landscape && <br/>}
 					{selected && landscape && nameOfThing}
 				</div>
